581cc04f90744bb55257da890335b22ff5374e9a
Shahid Raza <shahidraza962@gmail.com>
2023-12-28 13:01:18 +0530

    updated ls.sh

--- ls.sh
+++ ls.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # List all files in the current directory
+echo "List all files in the current directory"
 ls -l
+echo "-----------------------------"
 
 #read file content
+echo "Read file content"
 cat src/app.py
+echo "-----------------------------"
